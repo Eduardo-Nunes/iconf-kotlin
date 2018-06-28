d88234b4f78649aa6c995cea98348d6cd2b36141
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-06-28 08:51:49 -0300

    add coroutines and kotlinx experimental features

---
+++ src/_06_coroutines/ThreadsVsCoroutines.kt
@@ -0,0 +1,32 @@
+package _06_coroutines
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+fun main(args: Array<String>) = coroutines(100_000)
+
+fun threads(n: Int) {
+    val threads = List(n) {
+        thread {
+            sleep(1000L)
+            println(it)
+        }
+    }
+
+    threads.forEach { it.join() }
+}
+
+
+fun coroutines(n: Int) = runBlocking {
+    val jobs = List(100_000) {
+        async {
+            delay(1000L)
+            println(it)
+        }
+    }
+
+    jobs.forEach { it.join() }
+}
