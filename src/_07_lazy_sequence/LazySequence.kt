1b537d9ca3df10face863e74755217efa93fdfd2
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-06-28 19:13:00 -0300

    lazy sequence

---
+++ src/_07_lazy_sequence/LazySequence.kt
@@ -0,0 +1,20 @@
+package _07_lazy_sequence
+
+import kotlin.coroutines.experimental.buildSequence
+
+fun main(args: Array<String>) {
+    val seq = buildSequence {
+        var a = 1
+        var b = 1
+
+        while (true){
+            yield(a)
+            val tmp = a
+            a = b
+            b += tmp
+        }
+    }
+    println(
+            seq.take(20).toList()
+    )
+}
